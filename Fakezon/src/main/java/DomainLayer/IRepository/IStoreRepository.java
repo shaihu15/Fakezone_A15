d2b5cc8605866e9f449336586007afd898feb520
Shai Manor <mshai@post.bgu.ac.il>
2025-04-14 19:37:00 +0300

    IStoreRepository the interface is abstract the logic will be in the store

--- Fakezon/src/main/java/DomainLayer/IRepository/IStoreRepository.java
+++ Fakezon/src/main/java/DomainLayer/IRepository/IStoreRepository.java
@@ -1,4 +1,10 @@
 package DomainLayer.IRepository;
+import DomainLayer.Model.Store;
+import java.util.Collection;
 
 public interface IStoreRepository {
+    Store findById(String storeID);
+    Collection<Store> getAllStores();
+    void addStore(Store store);
+    void delete(String storeID);
 }
