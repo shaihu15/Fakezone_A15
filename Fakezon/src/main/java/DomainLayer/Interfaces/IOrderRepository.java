8cff0806a492f7761fe59b8dc44cc8e86ec7b0f0
Ron Yaacov <ronyaacov19@gamil.com>
2025-04-14 14:10:07 +0300

    add order repo interface and update the git ignore to ignore class files

--- Fakezon/src/main/java/DomainLayer/Interfaces/IOrderRepository.java
+++ Fakezon/src/main/java/DomainLayer/Interfaces/IOrderRepository.java
@@ -1,4 +1,8 @@
 package DomainLayer.Interfaces;
 
 public interface IOrderRepository {
+        void addOrder(String Order); // TODO: change the string to a IOrder object when the IOrder class is created
+        void updateOrder(int orderId, String Order); // TODO: change the string to a IOrder object when the IOrder class is created
+        void deleteOrder(int orderId);
+        String getOrder(int orderId); // TODO: when the IOrder interface is created, change the return type to IOrder
 }
