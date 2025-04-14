54f1f698cfd9dadcb3cabfdb61647731c274b24a
Ron Yaacov <ronyaacov19@gamil.com>
2025-04-14 13:58:27 +0300

    add default iml to product service

--- Fakezon/src/main/java/ApplicationLayer/Services/ProductService.java
+++ Fakezon/src/main/java/ApplicationLayer/Services/ProductService.java
@@ -1,4 +1,38 @@
 package ApplicationLayer.Services;
 
-public class ProductService {
+import java.util.List;
+
+import ApplicationLayer.Interfaces.IProductService;
+
+public class ProductService implements IProductService {
+
+    @Override
+    public void addProduct(String productName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addProduct'");
+    }
+
+    @Override
+    public void updateProduct(int productId, String productName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateProduct'");
+    }
+
+    @Override
+    public void deleteProduct(int productId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteProduct'");
+    }
+
+    @Override
+    public String viewProduct(int productId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'viewProduct'");
+    }
+
+    @Override
+    public List<String> searchProducts(String keyword) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'searchProducts'");
+    }
 }
