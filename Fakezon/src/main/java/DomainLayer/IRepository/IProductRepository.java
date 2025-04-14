1312146013c531b0e853704d52dc917ff5f94885
Ron Yaacov <ronyaacov19@gamil.com>
2025-04-14 14:22:07 +0300

    add prodcutRepo interface and default iml, create TODO for changing parameters and return values

--- Fakezon/src/main/java/DomainLayer/IRepository/IProductRepository.java
+++ Fakezon/src/main/java/DomainLayer/IRepository/IProductRepository.java
@@ -1,4 +1,12 @@
 package DomainLayer.IRepository;
 
+import java.util.List;
+
 public interface IProductRepository {
+    // Define the methods that the ProductRepository should implement
+    void addProduct(String product); // TODO: change the parameter type to the IProduct object when the IProduct class is created
+    void updateProduct(String product); // TODO: change the parameter type to the IProduct object when the IProduct class is created
+    void deleteProduct(int productId);
+    String getProductById(int productId); //TODO: change the return type to the IProduct object when the IProduct class is created
+    List<String> getAllProducts(); // TODO: change the return type to List<IProduct> when the IProduct class is created
 }
