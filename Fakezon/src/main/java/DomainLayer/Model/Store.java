b0a5803cca90388c0647f1f32003705c1090a838
IllyHas <illy@post.bgu.ac.il>
2025-04-15 13:10:07 +0300

    rating store and product

--- Fakezon/src/main/java/DomainLayer/Model/Store.java
+++ Fakezon/src/main/java/DomainLayer/Model/Store.java
@@ -6,13 +6,13 @@ public class Store {
     private String name;
     private int storeID;
     private HashMap<Integer, Product> products;
-    private HashMap<Integer, StoreRating> ratings;
+    private HashMap<Integer, StoreRating> Sratings;
 
     public Store(String name, int storeID) {
         this.name = name;
         this.storeID = storeID;
         this.products = new HashMap<>();
-        this.ratings = new HashMap<>();
+        this.Sratings = new HashMap<>();
     }
 
     public String getName() {
@@ -20,6 +20,10 @@ public class Store {
     }
     //precondition: user is logged in and  previously made a purchase from the store - cheaked by service layer
     public void addRating(int userID, double rating, String comment) {
-        ratings.put(userID, new StoreRating(userID, rating, comment));
+        if (Sratings.containsKey(userID)) {
+            Sratings.get(userID).updateRating(rating, comment);
+        } else {
+            Sratings.put(userID, new StoreRating(userID, rating, comment));
+        }
     }
 }
