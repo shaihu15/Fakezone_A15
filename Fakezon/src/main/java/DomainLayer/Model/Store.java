d6f65aae4b5b262586ff6a8a29a9b35d2fb3965b
Shai Manor <mshai@post.bgu.ac.il>
2025-04-15 11:09:08 +0300

    Store repository implementaion

--- Fakezon/src/main/java/DomainLayer/Model/Store.java
+++ Fakezon/src/main/java/DomainLayer/Model/Store.java
@@ -1,4 +1,20 @@
 package DomainLayer.Model;
-
+//just basic store class to prevent errors will be change in the issue of store model
 public class Store {
+    private String id;
+    private String name;
+
+
+    public Store(String id, String name) {
+        this.id = id;
+  
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
