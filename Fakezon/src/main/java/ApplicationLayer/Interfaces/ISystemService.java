2a730aeb7ec67bc714641dc3d10ef21c15a42618
Amit Zarchi <amitzar44@gmail.com>
2025-04-15 19:13:23 +0300

    implemented ISystemService

---
+++ Fakezon/src/main/java/ApplicationLayer/Interfaces/ISystemService.java
@@ -0,0 +1,15 @@
+package ApplicationLayer.Interfaces;
+
+import DomainLayer.Interfaces.IDelivery;
+import DomainLayer.Interfaces.IAuthenticator;
+import DomainLayer.Interfaces.IPayment;
+
+public interface ISystemService {
+    // Access to core services
+    IDelivery getDeliveryService();
+    IAuthenticator getAuthenticatorService();
+    IPayment getPaymentService();
+
+    void initialize();
+    void shutdown();
+}
