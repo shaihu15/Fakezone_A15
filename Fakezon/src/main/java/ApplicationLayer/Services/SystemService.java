d10a342ca416c7b9a8e1046a6ca98c5aa6e3ec75
Amit Zarchi <amitzar44@gmail.com>
2025-04-15 19:20:38 +0300

    implement service

--- Fakezon/src/main/java/ApplicationLayer/Services/SystemService.java
+++ Fakezon/src/main/java/ApplicationLayer/Services/SystemService.java
@@ -1,4 +1,49 @@
 package ApplicationLayer.Services;
 
-public class SystemService {
+import ApplicationLayer.Interfaces.ISystemService;
+import DomainLayer.Interfaces.IDelivery;
+import DomainLayer.Interfaces.IAuthenticator;
+import DomainLayer.Interfaces.IPayment;
+import InfrastructureLayer.Adapters.DeliveryAdapter;
+import InfrastructureLayer.Adapters.AuthenticatorAdapter;
+import InfrastructureLayer.Adapters.PaymentAdapter;
+
+public class SystemService implements ISystemService {
+    private IDelivery deliveryService;
+    private IAuthenticator authenticatorService;
+    private IPayment paymentService;
+
+    public SystemService() {
+        initialize();
+    }
+
+    @Override
+    public IDelivery getDeliveryService() {
+        return deliveryService;
+    }
+
+    @Override
+    public IAuthenticator getAuthenticatorService() {
+        return authenticatorService;
+    }
+
+    @Override
+    public IPayment getPaymentService() {
+        return paymentService;
+    }
+
+    @Override
+    public void initialize() {
+        this.deliveryService = new DeliveryAdapter();
+        this.authenticatorService = new AuthenticatorAdapter();
+        this.paymentService = new PaymentAdapter();
+    }
+
+    @Override
+    public void shutdown() {
+        // Clean up resources if needed
+        this.deliveryService = null;
+        this.authenticatorService = null;
+        this.paymentService = null;
+    }
 }
