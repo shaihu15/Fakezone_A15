7c5dd41c158e2f07019415be8e0f8c2b45b770ad
Amit Zarchi <amitzar44@gmail.com>
2025-04-16 12:37:12 +0300

    init spring boot

---
+++ Fakezon/src/main/java/com/fakezone/fakezone/FakezoneApplication.java
@@ -0,0 +1,13 @@
+package com.fakezone.fakezone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakezoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakezoneApplication.class, args);
+	}
+
+}
---
+++ Fakezon/src/test/java/com/fakezone/fakezone/FakezoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakezone.fakezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakezoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
