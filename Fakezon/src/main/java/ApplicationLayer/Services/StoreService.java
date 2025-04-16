fbf3592f4e4f2fc408f2027b5972288acfd53645
Escobachar <bacyuv@bgu.ac.il>
2025-04-16 15:02:45 +0300

    added addStoreOwner to storeservice

--- Fakezon/src/main/java/ApplicationLayer/Services/StoreService.java
+++ Fakezon/src/main/java/ApplicationLayer/Services/StoreService.java
@@ -1,4 +1,12 @@
 package ApplicationLayer.Services;
 
+import DomainLayer.IRepository.IStoreRepository;
+
 public class StoreService {
+    IStoreRepository storeRepository;
+    public void addStoreOwner(int storeId, int requesterId, int newOwnerId){
+        storeRepository.addStoreOwner(storeId, requesterId, newOwnerId);
+    }
+
+
 }
