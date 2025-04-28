19fb4e514466c2295fbb8827034716bd23637ad3
Ron Yaacov <ronyaacov19@gamil.com>
2025-04-28 12:53:54 +0300

    add option for error code

--- Fakezon/src/main/java/ApplicationLayer/Response.java
+++ Fakezon/src/main/java/ApplicationLayer/Response.java
@@ -1,14 +1,29 @@
 package ApplicationLayer;
 
+import ApplicationLayer.Enums.ErrorType;
+
 public class Response<T> {
     private final T data;
     private final String message;
     private final boolean success;
+    private final ErrorType errorType;
 
     public Response(T data, String message, boolean success) {
         this.data = data;
         this.message = message;
         this.success = success;
+        this.errorType = null;
+    }
+
+    public Response(T data, String message, boolean success, ErrorType errorType) {
+        this.data = data;
+        this.message = message;
+        this.success = success;
+        if(success) {
+            this.errorType = null;
+        } else {
+            this.errorType = errorType;
+        }
     }
 
     public T getData() {
