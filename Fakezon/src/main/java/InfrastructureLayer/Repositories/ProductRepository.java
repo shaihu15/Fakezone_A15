54bb8cbb8185d4c318878516839c96d780d86063
Ron Yaacov <ronyaacov19@gamil.com>
2025-05-04 14:53:59 +0300

    fix the update product

--- Fakezon/src/main/java/InfrastructureLayer/Repositories/ProductRepository.java
+++ Fakezon/src/main/java/InfrastructureLayer/Repositories/ProductRepository.java
@@ -3,6 +3,7 @@ package InfrastructureLayer.Repositories;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Set;
 
 import DomainLayer.IRepository.IProductRepository;
 import DomainLayer.Interfaces.IProduct;
@@ -24,13 +25,29 @@ public class ProductRepository implements IProductRepository {
     }
 
     @Override
-    public void updateProduct(IProduct product) {
-        IProduct currentProduct = products.get(product.getId());
+    public void updateProduct(int productId, String productName, String productDescription, Set<Integer> storesIds) {
+        IProduct currentProduct = products.get(productId);
         if(currentProduct == null){
             throw new IllegalArgumentException("Product not found in the repository.");
         }
-        currentProduct.setName(product.getName());
-        products.put(product.getId(), currentProduct);
+        if(productName != null && !productName.isEmpty()){
+            currentProduct.setName(productName);
+        }
+        if(productDescription != null){
+            currentProduct.setDescription(productDescription);
+        }
+        if(storesIds != null){
+            for(Integer storeId : storesIds){
+                if(!currentProduct.getStoresIds().contains(storeId)){
+                    currentProduct.addStore(storeId);
+                }
+                else{
+                    currentProduct.removeStore(storeId);
+                }
+            }
+        }
+
+        products.put(productId, currentProduct);
     }
 
     @Override
