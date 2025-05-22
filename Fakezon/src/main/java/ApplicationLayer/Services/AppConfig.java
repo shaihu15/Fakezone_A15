c8c854f879d1213d3ba13366fcfd91bc6f77904b
IllyHas <illy@post.bgu.ac.il>
2025-05-22 16:01:06 +0300

    remove unused imports

--- Fakezon/src/main/java/ApplicationLayer/Services/AppConfig.java
+++ Fakezon/src/main/java/ApplicationLayer/Services/AppConfig.java
@@ -1,24 +1,8 @@
 package ApplicationLayer.Services;
 
-import ApplicationLayer.Interfaces.IStoreService;
-import ApplicationLayer.Interfaces.INotificationWebSocketHandler;
-import ApplicationLayer.Interfaces.ISystemService;
-import ApplicationLayer.Interfaces.IUserService;
-import DomainLayer.IRepository.IProductRepository;
-import DomainLayer.IRepository.IStoreRepository;
-import DomainLayer.IRepository.IUserRepository;
-import DomainLayer.Interfaces.IOrderRepository;
-import InfrastructureLayer.Adapters.AuthenticatorAdapter;
-import InfrastructureLayer.Adapters.NotificationWebSocketHandler;
-import InfrastructureLayer.Repositories.OrderRepository;
-import InfrastructureLayer.Repositories.ProductRepository;
-import InfrastructureLayer.Repositories.StoreRepository;
-import InfrastructureLayer.Repositories.UserRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.ApplicationEventPublisher;
 
 @Configuration
 public class AppConfig {
-   
+
 }
