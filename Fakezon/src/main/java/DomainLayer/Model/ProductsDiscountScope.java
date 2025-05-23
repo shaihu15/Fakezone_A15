762e9159997361b70d4f98140bf75e47c6e48aab
Amit Zarchi <amitzar44@gmail.com>
2025-05-23 18:38:38 +0300

    store and product discountn scope

---
+++ Fakezon/src/main/java/DomainLayer/Model/ProductsDiscountScope.java
@@ -0,0 +1,42 @@
+package DomainLayer.Model;
+
+import DomainLayer.Interfaces.IDiscountScope;
+
+import java.util.List;
+import java.util.Map;
+
+public class ProductsDiscountScope implements IDiscountScope {
+    private List<Integer> productIds;
+    private int storeId;
+    private Map<Integer, StoreProduct> storeProducts;
+    
+    public ProductsDiscountScope(List<Integer> productIds, int storeId, Map<Integer, StoreProduct> storeProducts) {
+        this.productIds = productIds;
+        this.storeId = storeId;
+        this.storeProducts = storeProducts;
+    }
+
+
+    public List<Integer> getProductIds() {
+        return productIds;
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    @Override
+    public double getEligibleAmount(Cart cart) {
+        Map<Integer,Map<Integer,Integer>> allProducts = cart.getAllProducts();
+        double eligibleAmount = 0;
+        for (Map.Entry<Integer, Map<Integer, Integer>> entry : allProducts.entrySet()) {
+            Map<Integer, Integer> products = entry.getValue();
+            if (productIds.contains(entry.getKey()) && entry.getKey() == storeId) {
+                for (Map.Entry<Integer, Integer> product : products.entrySet()) {
+                    eligibleAmount += product.getValue() * storeProducts.get(product.getKey()).getBasePrice();
+                }
+            }
+        }
+        return eligibleAmount;
+    }
+}
