b90df54d9494861d4302c34ee2510d5260069023
Amit Zarchi <amitzar44@gmail.com>
2025-05-23 18:40:14 +0300

    discount with 1 condition

---
+++ Fakezon/src/main/java/DomainLayer/Model/ConditionDiscount.java
@@ -0,0 +1,36 @@
+package DomainLayer.Model;
+
+import DomainLayer.Interfaces.IDiscountPolicy;
+import DomainLayer.Interfaces.IDiscountScope;
+import java.util.function.Predicate;
+public class ConditionDiscount implements IDiscountPolicy {
+    private int policyID;
+    private double percentage;
+    private IDiscountScope scope;
+    private Predicate<Cart> condition;
+
+    public ConditionDiscount(int policyID, double percentage, IDiscountScope scope, Predicate<Cart> condition) {  
+        this.policyID = policyID;
+        this.percentage = percentage;
+        this.scope = scope;
+        this.condition = condition;
+    }
+
+    @Override
+    public double apply(Cart cart) {
+        if (!condition.test(cart)) {
+            return 0;
+        }
+        return scope.getEligibleAmount(cart) * percentage / 100;
+    }
+
+    @Override
+    public boolean isApplicable(Cart cart) {
+        return condition.test(cart);
+    }
+    
+    @Override
+    public int getPolicyID() {
+        return policyID;
+    }
+}
